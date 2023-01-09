507d0e801aaa6c240885d7eb2021fc27d5c37a7c
Alvaro Fernandez <alvaro.fernandez@transgourmet.ch>
2023-01-09 16:10:05 +0100

    credit data cleaning

---
+++ loandefault/analysis/clean_data.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+# clean train dataset
+df = pd.read_csv('../data/train.csv')
+
+df = df.drop('Unnamed: 0', axis=1)
+zip_code = pd.get_dummies(df['ZIP'])
+df = pd.concat([df, zip_code], axis=1)
+df = df.drop('ZIP', axis=1)
+df = df.rename(columns={'MT01RA': 'zip0', 'MT04PA': 'zip1', 'MT12RA': 'zip2', 'MT15PA': 'zip3'})
+
+occup = pd.get_dummies(df['occupation'])
+df = pd.concat([df, occup], axis=1)
+df = df.drop('occupation', axis=1)
+df = df.rename(columns={'MZ01CD': 'occupation0', 'MZ10CD': 'occupation1', 'MZ11CD': 'occupation2'})
+
+df.to_csv('../data/train_clean.csv', encoding='utf-8', index=False)
+
+#####
+
+# clean test dataset
+df = pd.read_csv('../data/test.csv')
+
+df = df.drop('Unnamed: 0', axis=1)
+df = df.drop('Unnamed: 0.1', axis=1)
+
+zip_code = pd.get_dummies(df['ZIP'])
+df = pd.concat([df, zip_code], axis=1)
+df = df.drop('ZIP', axis=1)
+df = df.rename(columns={'MT01RA': 'zip0', 'MT04PA': 'zip1', 'MT12RA': 'zip2', 'MT15PA': 'zip3'})
+
+occup = pd.get_dummies(df['occupation'])
+df = pd.concat([df, occup], axis=1)
+df = df.drop('occupation', axis=1)
+df = df.rename(columns={'MZ01CD': 'occupation0', 'MZ10CD': 'occupation1', 'MZ11CD': 'occupation2'})
+
+df['target'] = df['default'].astype(int)
+df = df.drop('default', axis=1)
+
+df.to_csv('../data/test_clean.csv', encoding='utf-8', index=False)
