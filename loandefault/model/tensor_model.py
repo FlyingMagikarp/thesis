4ef2c9ee7cd9a7611f9e92c181fd1f9a333a2a5b
Alvaro Fernandez <alvaro.fernandez@transgourmet.ch>
2023-02-10 16:10:02 +0100

    better loan model, still bad tests

--- loandefault/model/tensor_model.py
+++ loandefault/model/tensor_model.py
@@ -8,39 +8,39 @@ df = pd.read_csv('../data/train_clean.csv')
 print(df.head())
 print(df.dtypes)
 
-#df = df.drop('Female', axis=1)
+df_val = pd.read_csv('../data/test_clean.csv')
+y_val = df_val.pop('target')
+X_val = df_val
+
 y = df.pop('target')
 X = df
 
 X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.2, random_state=129)
 
-
-#tensor = tf.convert_to_tensor(df)
-
 model = tf.keras.models.Sequential()
 
 model.add(tf.keras.layers.Dense(17, activation='relu', input_shape=(17,)))
 
-model.add(tf.keras.layers.Dense(40, activation='relu'))
-model.add(tf.keras.layers.Dense(40, activation='relu'))
-model.add(tf.keras.layers.Dense(40, activation='relu'))
-model.add(tf.keras.layers.Dense(40, activation='relu'))
+model.add(tf.keras.layers.Dense(40, activation=tf.keras.layers.LeakyReLU(alpha=0.01)))
+model.add(tf.keras.layers.Dense(40, activation=tf.keras.layers.LeakyReLU(alpha=0.01)))
+model.add(tf.keras.layers.Dense(40, activation=tf.keras.layers.LeakyReLU(alpha=0.01)))
+model.add(tf.keras.layers.Dense(40, activation=tf.keras.layers.LeakyReLU(alpha=0.01)))
 
-model.add(tf.keras.layers.Dense(40, activation='relu'))
-model.add(tf.keras.layers.Dense(40, activation='relu'))
-model.add(tf.keras.layers.Dense(40, activation='relu'))
+model.add(tf.keras.layers.Dense(40, activation=tf.keras.layers.LeakyReLU(alpha=0.01)))
+model.add(tf.keras.layers.Dense(40, activation=tf.keras.layers.LeakyReLU(alpha=0.01)))
+model.add(tf.keras.layers.Dense(40, activation=tf.keras.layers.LeakyReLU(alpha=0.01)))
 model.add(tf.keras.layers.Dense(40, activation='relu'))
 
 model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', tf.keras.metrics.Recall()])
 
-val_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+val_dataset = tf.data.Dataset.from_tensor_slices((X_val, y_val))
 val_dataset = val_dataset.batch(64)
 
 
-model.fit(X_train, y_train, epochs=33, batch_size=64, validation_data=val_dataset)
-results_first = model.evaluate(X_test,  y_test, verbose=2)
+model.fit(X, y, epochs=33, batch_size=64, validation_data=val_dataset)
+results_first = model.evaluate(X_val, y_val, verbose=2)
 print(results_first)
 
 model.save('./models/baseline')
